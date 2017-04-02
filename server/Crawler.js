d96f3fce57e1320082b72860b40d96c79b3e7e7c
Roselma Mendes <roselma.mendes@gmail.com>
2017-04-02 11:19:46 -0300

    Get the metadata of the links of one tag from the server.

---
+++ server/Crawler.js
@@ -0,0 +1,37 @@
+import metafetch from 'metafetch';
+
+export default class Crawler{
+
+  static promise_read_links(link){
+    return new Promise((resolve, reject) => {
+      metafetch.fetch(link, {}, (error, data) => {
+        if(error){
+          reject(error);
+        }else {
+          console.info(link);
+          resolve(data);
+        }
+
+      });
+    });
+  }
+
+  static promise_build_tag_links(tag){
+    return new Promise((resolve, reject) => {
+
+      var links_promises = tag.links.map(Crawler.promise_read_links);
+      return Promise.all(links_promises).then((links_data) => {
+        tag.links = links_data;
+        resolve(tag);
+      }).catch(error => {
+        throw error;
+      });
+
+    });
+  }
+
+  static build_tag_links(tags){
+    return tags.map(Crawler.promise_build_tag_links);
+  }
+
+};
---
+++ server/app.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import path from 'path';
+let app = express();
+import Crawler from './Crawler';
+
+const tags = [
+  {
+    "tag": "Segurança",
+    "links": [
+      "https://medium.com/@roselmamendes/todas-as-pessoas-deveriam-saber-o-que-%C3%A9-seguran%C3%A7a-digital-59ea745a4763#.ax61l6p4c"
+    ]
+  },
+  {
+    "tag": "Testes em JS",
+    "links": [
+      "http://sinonjs.org/"
+    ]
+  }
+];
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/tags-list', (req, res, next) => {
+  Promise.all(Crawler.build_tag_links(tags)).then((tags) => {
+
+    var tags_json = tags.map((tag) => {
+      return {
+        'title': tag.tag,
+        'links': tag.links.map((link) => {
+          return {
+            'title': link.title,
+            'image': link.image,
+            'url': link.url
+          }
+        })
+      }
+    });
+
+
+    res.send(tags_json);
+  }).catch(next);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(3000, () => {
+  console.log('link-aggregator listening on port 3000!');
+});
